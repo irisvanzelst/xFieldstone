1eab358076e11ae468df46168f3c09141c3c7ed1
Iris van Zelst <iris.vanzelst@erdw.ethz.ch>
2021-02-16 16:58:33 +0100

    new viscosity for dislocation and diffusion creep for case 2c

--- main_solver_Stokes_T_grid.py
+++ main_solver_Stokes_T_grid.py
@@ -126,10 +126,7 @@ def viscosity(exx,eyy,exy,T,imat):
        eta_disl=input.eta_def  
 
     # combining dislocation and diffusion creep
-    if eta_disl <= eta_diff:
-       eta_comb = eta_disl
-    else:
-       eta_comb = eta_diff 
+    eta_comb = (2*eta_disl*eta_diff) / (eta_diff + eta_disl)
 
     if input.case=='1a' or input.case=='1b' or input.case=='1c':
        val=input.eta_def
